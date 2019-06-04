171648f1509e7586d0d0fbaf2c5731791647aa98
simeng.he <simeng.he@plasticmobile.com>
2019-06-04 16:52:29 -0400

    rat

--- app/src/main/java/com/example/boardroombooking/MainActivity.kt
+++ app/src/main/java/com/example/boardroombooking/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.boardroombooking
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.content.pm.ActivityInfo
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
@@ -9,20 +11,34 @@ import android.support.annotation.RequiresApi
 import android.support.design.widget.Snackbar
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.widget.Toast
+import com.android.volley.toolbox.JsonObjectRequest
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import okhttp3.*
+import org.json.JSONObject
 import java.io.IOException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import org.json.JSONException
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+
 const val datePat = "dd MMMM yyyy"
 const val timePat = "HH:mm"
 const val username = "ratnu"
 const val password = "mandem"
 var location = "Alcorn"
-                                                                                                
+const val pattern1 = "MM/dd/yyyy, HH:mm:ss"
+
 class MainActivity : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -31,7 +47,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recycle_main.layoutManager= LinearLayoutManager(this)
-        recycle_main.adapter =  MainAdapter() //Run the the recycler view code.
+        fetchJson()
         val curr =  LocalDateTime.now()
         val dateFormatter = DateTimeFormatter.ofPattern(datePat)
         val timeFormat = DateTimeFormatter.ofPattern(timePat)
@@ -45,28 +61,107 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,"Creating a meeting!",Toast.LENGTH_SHORT).show()
             fetchJson()
         }
+    }
+    fun strToDate(date:String?,pattern:String? = pattern1): Date { //Changes the string to a date object.
+        return SimpleDateFormat(pattern).parse(date)
 
-
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun sortByTime(dataList:ArrayList<Data>):ArrayList<Data>{
+        //also remove entries that are not in the day...
+        val curr =  LocalDateTime.now()
+        val dateFormatter = DateTimeFormatter.ofPattern(datePat)
+        val curDate = curr.format(dateFormatter)
+        val laterDate = curr.plusDays(1).format(dateFormatter)
+        println("\n THE DATE IS "+curDate)
+        println("\n THE LATE IS "+laterDate)
+        val pattern2 = "dd MMMMM yyyy"
+        //The dates that are shown in the app must be between these two.
+        val todayStart = strToDate(curDate,pattern2)
+        val todayEnd = strToDate(laterDate,pattern2)
+        println(todayStart)
+        println(todayEnd)
+        val newData = ArrayList<Data>()
+        dataList.sortBy { strToDate(it.startingTime) }
+        dataList.forEach {
+            println(it.startingTime)
+            val startTime = strToDate(it.startingTime)
+            if(startTime>=todayStart&&startTime<=todayEnd){
+                println("ALLOWED")
+                newData.add(it)
+            }
+        }
+        newData.forEach {
+            println("AYY")
+            println(it.startingTime)
+        }
+        return newData
     }
     fun fetchJson(){ //function to fetch json from the web.
+        var keys:Iterator<String>
         println("FETCHING JSON")
-        val url = "https://ratnuback.appspot.com/getData"
+        //val url = "https://api.myjson.com/bins/9wzmf"
+        var url ="https://ratnuback.appspot.com/getBooking/Shaftesbury"
         val credential = Credentials.basic(username, password);
         val request = Request.Builder().url(url).header("Authorization",credential).build()
-
         val client = OkHttpClient()
+        var dataList = ArrayList<Data>()
         client.newCall(request).enqueue(object:Callback{
             override fun onFailure(call: Call, e: IOException) {
-                println("epic fail")
+                println("fail")
             }
-
+            @RequiresApi(Build.VERSION_CODES.O)
             override fun onResponse(call: Call, response: Response) {
-                val body = response?.body?.string()
-                println(body)
-                val gson = GsonBuilder().create()
-                txt_instructions.setText(body)
+                val body = response.body?.string()
+                try {
+                    val jsonObject = JSONObject(body)
+                    val gson = GsonBuilder().create()
+                    jsonObject.keys().forEach {
+                       // println("key: $it")
+                        //println(jsonObject[it].toString())
+                        dataList.add(gson.fromJson(jsonObject[it].toString(), Data::class.java))
+                    }
+                } catch (err: JSONException) {
+                    Log.d("Error", err.toString())
+                }
+
+                dataList.forEach {
+                    println("title : ${it.title}")
+                }
+                dataList = sortByTime(dataList)
+                runOnUiThread{
+                    recycle_main.adapter =  MainAdapter(dataList) //Run the the recycler view code.
+                }
+                //val boardroom =  gson.fromJson(body,Object::class.java)
             }
         })
-
     }
 }
+    class Data (
+    @SerializedName("a")
+    val a:Content? = null,
+    @SerializedName("endingTime")
+    val endingTime:String? = null,
+    @SerializedName("floor")
+    val floor:Int? = null,
+    @SerializedName("meetingRoom")
+    val meetingRoom:String?= null,
+    @SerializedName("meetingState")
+    val meetingState:String? = null,
+    @SerializedName("startingTime")
+    val startingTime:String? = null,
+    @SerializedName("title")
+    val title:String? = null,
+    @SerializedName("userName")
+    val userName:String? = null
+)
+
+
+
+class Content(
+    @SerializedName("description")
+    val description:String? = null,
+    @SerializedName("id")
+    val id:String? = null)
+class Desc(val desc:String,val time:String)
+
--- app/src/main/java/com/example/boardroombooking/MainAdapter.kt
+++ app/src/main/java/com/example/boardroombooking/MainAdapter.kt
@@ -8,25 +8,42 @@ import android.view.ViewGroup
 import okhttp3.OkHttpClient
 import android.R.string
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.bookingitem.view.*
+import org.json.JSONObject
 import okhttp3.Request
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
-class MainAdapter: RecyclerView.Adapter<CustomViewHolder>(){
+class MainAdapter(val dataList:ArrayList<Data>): RecyclerView.Adapter<CustomViewHolder>(){
+
     //trying a get request....
-    var resper = ""
-    val url = "https://ratnuback.appspot.com/"
     override fun getItemCount(): Int { //probably determined by a count through the database...
-        return 6
+        return dataList.count()
     }
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CustomViewHolder {
         val layoutInflater = LayoutInflater.from(p0.context)
         val cellForRow = layoutInflater.inflate(R.layout.bookingitem,p0,false)
-
         return CustomViewHolder(cellForRow)
     }
 
-    override fun onBindViewHolder(p0: CustomViewHolder, p1: Int) {
+    override fun onBindViewHolder(p0: CustomViewHolder, pos: Int) {
+        //example date: 5/30/2019, 16:00:00
+        val booking = dataList[pos]
+        val pattern = "MM/dd/yyyy, HH:mm:ss"
+        p0.itemView.txt_title?.text = "Title : "+ booking.title
+        p0.itemView.txt_name?.text = "Name : " + booking.userName
+        //should format the date using some java library.
+        val startTime = SimpleDateFormat(pattern).parse(booking.startingTime)
+        val endTime = SimpleDateFormat(pattern).parse(booking.endingTime)
+        println(startTime<endTime)
+        //println("\n"+startTime.toString())
+        //println("\n"+endTime.toString())
+        p0.itemView.txt_duration?.text ="Duration : " + booking.startingTime?.substring(10,16) + " - "+
+                booking.endingTime?.substring(10,16)
 
     }
 }
